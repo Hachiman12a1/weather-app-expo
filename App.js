d98ac5161e247daed74eb03944b4f6d4c28faaaf
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-08-25 21:48:07 +0700

    Navigation Bottom Tabs

--- App.js
+++ App.js
@@ -1,21 +1,64 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import City from "./src/screens/City";
 import CurrentWeather from "./src/screens/CurrentWeather";
 import UpcomingWeather from "./src/screens/UpcomingWeather";
-import City from "./src/screens/City";
+import { Feather } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <CurrentWeather />
-      {/* <UpcomingWeather /> */}
-      {/* <City /> */}
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: "#e91e63",
+        }}
+      >
+        <Tab.Screen
+          name="Current"
+          component={CurrentWeather}
+          options={{
+            tabBarLabel: "Current",
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="droplet"
+                size={25}
+                color={focused ? "#e91e63" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Upcoming"
+          component={UpcomingWeather}
+          options={{
+            tabBarLabel: "Upcoming",
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="clock"
+                size={25}
+                color={focused ? "#e91e63" : "black"}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="City"
+          component={City}
+          options={{
+            tabBarLabel: "City",
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                name="home"
+                size={25}
+                color={focused ? "#e91e63" : "black"}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
