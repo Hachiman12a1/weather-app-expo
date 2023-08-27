3b812a8e1e47d6e7600f356498f18a766eb7f609
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-08-27 10:53:04 +0700

    Custom Hooks useGetWeather to call API

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import Tabs from "./src/components/Tabs";
+import useGetWeather from "./src/hooks/useGetWeather";
 
 export default function App() {
+  const { loading, error, weatherData } = useGetWeather();
+
+  if (weatherData && weatherData.list) {
+    return (
+      <NavigationContainer>
+        <Tabs weatherData={weatherData} />
+      </NavigationContainer>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <Tabs />
-    </NavigationContainer>
+    <View style={style.container}>
+      <ActivityIndicator size={"large"} color={"blue"} />
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    flex: 1,
+  },
+});
---
+++ src/hooks/useGetWeather.js
@@ -0,0 +1,52 @@
+import { WEATHER_API_KEY } from "@env";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+
+function useGetWeather() {
+  const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState({
+    lat: [],
+    long: [],
+  });
+  const [error, setError] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
+
+  const fetchWeatherData = async () => {
+    try {
+      const res = await fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?lat=${location?.lat}&lon=${location?.long}&appid=${WEATHER_API_KEY}`
+      );
+      const data = await res.json();
+      setWeatherData(data);
+    } catch (error) {
+      setError("Could not fetch weather");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setError("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        lat: location.coords.latitude,
+        long: location.coords.longitude,
+      });
+      await fetchWeatherData();
+    })();
+  }, [location?.lat, location?.long]);
+
+  return {
+    loading,
+    error,
+    weatherData,
+  };
+}
+
+export default useGetWeather;
